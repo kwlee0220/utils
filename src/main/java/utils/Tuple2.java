4373f5dcc75a27ce3540bf08f6d6b6465a9bf19d
kwlee0220 <kwlee@KWLEE-OFFICE>
2017-03-06 18:56:05 +0900

    .

---
+++ src/main/java/utils/Tuple2.java
@@ -0,0 +1,16 @@
+package utils;
+
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public final class Tuple2<T1,T2> {
+	public final T1 _1;
+	public final T2 _2;
+	
+	public Tuple2(T1 _1, T2 _2) {
+		this._1 = _1;
+		this._2 = _2;
+	}
+}
