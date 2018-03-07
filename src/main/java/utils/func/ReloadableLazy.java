606430250193ef9e537bd1038b7743d00acf453f
kwlee <kwlee@etri.re.kr>
2018-03-07 18:00:18 +0900

    .

---
+++ src/main/java/utils/func/ReloadableLazy.java
@@ -0,0 +1,35 @@
+package utils.func;
+
+import java.util.function.Supplier;
+
+import io.vavr.control.Option;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class ReloadableLazy<T> {
+	private Option<T> m_loaded;
+	private Supplier<T> m_supplier;
+	
+	public static <T> ReloadableLazy<T> of(Supplier<T> supplier) {
+		return new ReloadableLazy<>(supplier);
+	}
+	
+	private ReloadableLazy(Supplier<T> supplier) {
+		m_loaded = Option.none();
+		m_supplier = supplier;
+	}
+	
+	public synchronized T get() {
+		if ( m_loaded.isEmpty() ) {
+			m_loaded = Option.some(m_supplier.get());
+		}
+		
+		return m_loaded.get();
+	}
+	
+	public synchronized void setDirty() {
+		m_loaded = Option.none();
+	}
+}
