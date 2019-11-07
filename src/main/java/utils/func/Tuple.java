b990e71095dfa574051944c5aa755721c28c2c72
kwlee <kwlee@etri.re.kr>
2019-11-07 23:13:36 +0900

    .

---
+++ src/main/java/utils/func/Tuple.java
@@ -0,0 +1,45 @@
+package utils.func;
+
+import java.util.Objects;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public final class Tuple<T1,T2> {
+	public final T1 _1;
+	public final T2 _2;
+	
+	public static <T1,T2> Tuple<T1,T2> of(T1 t1, T2 t2) {
+		return new Tuple<>(t1, t2);
+	}
+	
+	private Tuple(T1 t1, T2 t2) {
+		_1 = t1;
+		_2 = t2;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		else if ( obj == null || obj.getClass() != Tuple.class ) {
+			return false;
+		}
+		
+		@SuppressWarnings("unchecked")
+		Tuple<T1,T2> other = (Tuple<T1,T2>)obj;
+		return Objects.equals(_1, other._1) && Objects.equals(_2, other._2);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(_1, _2);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("(%s, %s)", _1, _2);
+	}
+}
