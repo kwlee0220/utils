12bf04e7d27ecea1746aa948eab55c81826bb8e6
kwlee <kwlee@node97.sbdata.etri.re.kr>
2020-07-18 14:09:20 +0900

    move Serializable-related methods to SerializableUtils

--- src/main/java/utils/io/IOUtils.java
+++ src/main/java/utils/io/IOUtils.java
@@ -1,10 +1,7 @@
 package utils.io;
 
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataOutput;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,33 +9,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.Reader;
-import java.io.Serializable;
 import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.Base64;
 import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
-import com.google.common.collect.Lists;
 import com.google.common.io.CharStreams;
 
 import utils.Utilities;
 import utils.async.AbstractThreadedExecution;
 import utils.async.CancellableWork;
-import utils.func.FOption;
 import utils.func.Tuple;
 
 /**
@@ -249,105 +239,12 @@ public class IOUtils {
 		return output;  
 	}
 	
-	public static String toSerializedString(Serializable obj) throws IOException {
-		return stringify(serialize(obj));
-	}
-	
-	public static Object fromSerializedString(String encoded)
-		throws IOException, ClassNotFoundException {
-		return deserialize(destringify(encoded));
-	}
-	
-	public static byte[] serialize(Serializable obj) throws IOException {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try ( ObjectOutputStream oos = new ObjectOutputStream(baos) ) {
-			oos.writeObject(obj);
-		}
-		baos.close();
-		
-		return baos.toByteArray();
-	}
-	
-	public static Object deserialize(byte[] serialized)
-		throws IOException, ClassNotFoundException {
-		try ( ByteArrayInputStream bais = new ByteArrayInputStream(serialized);
-				ObjectInputStream ois = new ObjectInputStream(bais); ) {
-			return ois.readObject();
-		}
-	}
-	
 	public static String stringify(byte[] bytes) {
 		return Base64.getEncoder().encodeToString(bytes);
 	}
 	
 	public static byte[] destringify(String encoded) {
 		return Base64.getDecoder().decode(encoded);
-	}
-	
-	public static void serializeOption(FOption<? extends Serializable> obj,
-										ObjectOutputStream oos) throws IOException {
-		if ( obj.isPresent() ) {
-			oos.writeBoolean(true);
-			oos.writeObject(obj.get());
-		}
-		else {
-			oos.writeBoolean(false);
-		}
-	}
-	
-	public static void writeOptionDouble(FOption<Double> opt,
-										DataOutput out) throws IOException {
-		if ( opt.isPresent() ) {
-			out.writeBoolean(true);
-			out.writeDouble(opt.get());
-		}
-		else {
-			out.writeBoolean(false);
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T extends Serializable>
-	FOption<T> deserializeOption(ObjectInputStream ois, Class<T> cls) throws ClassNotFoundException, IOException {
-		return (ois.readBoolean())
-				? FOption.of((T)ois.readObject())
-				: FOption.empty();
-	}
-
-	public static <T extends Serializable>
-	void serializeList(List<T> list, ObjectOutputStream oos) throws IOException {
-		oos.writeInt(list.size());
-		for ( T item: list ) {
-			oos.writeObject(item);
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <T extends Serializable>
-	List<T> deserializeList(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-		int count = ois.readInt();
-		List<T> list = Lists.newArrayListWithExpectedSize(count);
-		for ( int i =0; i < count; ++i ) {
-			list.add((T)ois.readObject());
-		}
-		
-		return list;
-	}
-	
-	public static void readStringCollection(Consumer<String> consumer, DataInput in)
-		throws IOException {
-		int count = in.readInt();
-		for ( int i =0; i < count; ++i ) {
-			consumer.accept(in.readUTF());
-		}
-	}
-	
-	public static void writeStringCollection(Collection<String> coll,
-											DataOutput out) throws IOException {
-		out.writeInt(coll.size());
-		for ( String item: coll ) {
-			out.writeUTF(item);
-		}
 	}
 	
 	public static Tuple<PipedOutputStream, PipedInputStream> pipe(int pipeSize) {
