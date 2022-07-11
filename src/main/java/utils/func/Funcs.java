5ddb2b562ad0e8f9eaac5a4bfb9defbe11d01d47
Kang-Woo Lee <kwlee@etri.re.kr>
2022-07-11 13:19:56 +0900

    Set functional interface 추가

--- src/main/java/utils/func/Funcs.java
+++ src/main/java/utils/func/Funcs.java
@@ -1,9 +1,12 @@
 package utils.func;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
+
+import com.google.common.collect.Sets;
 
 /**
  * 
@@ -61,5 +64,17 @@ public class Funcs {
 		if ( value != null ) {
 			consumer.accept(key, value);
 		}
+	}
+	
+	public static <T> Set<T> add(Set<T> set, T elm) {
+		Set<T> added = Sets.newHashSet(set);
+		added.add(elm);
+		return added;
+	}
+	
+	public static <T> Set<T> remove(Set<T> set, T elm) {
+		Set<T> removed = Sets.newHashSet(set);
+		removed.remove(elm);
+		return removed;
 	}
 }
