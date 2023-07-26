2f8c10c563b02a71d1bb6f00a3af234bd832aa76
'kwlee' <'kwlee@etri.re.kr'>
2023-07-26 11:16:48 +0900

    initial import.

---
+++ src/main/java/utils/Indexed.java
@@ -0,0 +1,47 @@
+package utils;
+
+import com.google.common.base.Objects;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public final class Indexed<T> {
+	private final int m_index;
+	private final T m_data;
+	
+	public static <T> Indexed<T> of(int index, T data) {
+		return new Indexed<>(index, data);
+	}
+	
+	private Indexed(int index, T data) {
+		m_index = index;
+		m_data = data;
+	}
+	
+	public int getIndex() {
+		return m_index;
+	}
+	
+	public T getData() {
+		return m_data;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(m_index, m_data);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		else if ( obj == null  || obj.getClass() != Indexed.class ) {
+			return false;
+		}
+		
+		Indexed<T> other = (Indexed)obj;
+		return m_index == other.m_index && m_data.equals(other.m_data);
+	}
+}
