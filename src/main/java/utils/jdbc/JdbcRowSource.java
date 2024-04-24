ec9d636b4c95767de09fedbef113811572a21f37
'kwlee' <'kwlee@etri.re.kr'>
2024-04-24 15:27:08 +0900

    Builder pattern 수정.

--- src/main/java/utils/jdbc/JdbcRowSource.java
+++ src/main/java/utils/jdbc/JdbcRowSource.java
@@ -4,9 +4,11 @@ import static utils.Utilities.checkState;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import utils.func.CheckedFunction;
+import utils.Throwables;
+import utils.func.CheckedFunctionX;
 import utils.func.FOption;
 import utils.func.Tuple;
 import utils.func.Tuple3;
@@ -26,73 +28,48 @@ import io.reactivex.rxjava3.core.ObservableOnSubscribe;
  * @author Kang-Woo Lee (ETRI)
  */
 public class JdbcRowSource<T> {
-	private JdbcProcessor m_proc;
+	private final JdbcProcessor m_proc;
 	private Connection m_conn;
 	private ResultSet m_rs;
-	private CheckedFunction<Connection, ResultSet> m_exector;
-	private CheckedFunction<ResultSet, T> m_deser;
+	private final CheckedFunctionX<Connection, ResultSet, SQLException> m_exector;
+	private final CheckedFunctionX<ResultSet, T, SQLException> m_deser;
 	
-	private JdbcRowSource(CheckedFunction<ResultSet, T> deserializer) {
+	private JdbcRowSource(JdbcProcessor proc, Connection conn, ResultSet rs,
+							CheckedFunctionX<Connection, ResultSet, SQLException> exector,
+							CheckedFunctionX<ResultSet, T, SQLException> deserializer) {
+		m_proc = proc;
+		m_conn = conn;
+		m_rs = rs;
+		m_exector = exector;
 		m_deser = deserializer;
 	}
 	
-	private static CheckedFunction<ResultSet, List<Object>> deserializeAsObjectList() {
-		return rs -> FStream.range(1, rs.getMetaData().getColumnCount()+1)
-							.mapOrThrow(idx -> rs.getObject(idx))
-							.toList();
+	public static <T> WaitRowSource<T> select(CheckedFunctionX<ResultSet, T, SQLException> deserializer) {
+		return new WaitRowSource<>(deserializer);
 	}
 	
-	public static <T> JdbcRowSource<T> select(CheckedFunction<ResultSet, T> deserializer) {
-		return new JdbcRowSource<>(deserializer);
+	public static WaitRowSource<List<Object>> selectAsValueList() {
+		return new WaitRowSource<>(deserializeAsObjectList());
 	}
 	
-	public static JdbcRowSource<List<Object>> selectAsValueList() {
-		return new JdbcRowSource<>(deserializeAsObjectList());
+	public static WaitRowSource<Object> selectAsObject() {
+		return new WaitRowSource<>(rs -> rs.getObject(1));
+	}
+	public static WaitRowSource<String> selectAsString() {
+		return new WaitRowSource<>(rs -> rs.getString(1));
+	}
+	public static WaitRowSource<Integer> selectAsInt() {
+		return new WaitRowSource<>(rs -> rs.getInt(1));
+	}
+	public static WaitRowSource<Long> selectAsLong() {
+		return new WaitRowSource<>(rs -> rs.getLong(1));
 	}
 	
-	public static JdbcRowSource<Object> selectAsObject() {
-		return new JdbcRowSource<>(rs -> rs.getObject(1));
+	public static WaitRowSource<Tuple<Object,Object>> selectAsTuple2() {
+		return new WaitRowSource<>(rs -> Tuple.of(rs.getObject(1), rs.getObject(2)));
 	}
-	public static JdbcRowSource<String> selectAsString() {
-		return new JdbcRowSource<>(rs -> rs.getString(1));
-	}
-	public static JdbcRowSource<Integer> selectAsInt() {
-		return new JdbcRowSource<>(rs -> rs.getInt(1));
-	}
-	public static JdbcRowSource<Long> selectAsLong() {
-		return new JdbcRowSource<>(rs -> rs.getLong(1));
-	}
-	
-	public static JdbcRowSource<Tuple<Object,Object>> selectAsTuple2() {
-		return new JdbcRowSource<>(rs -> Tuple.of(rs.getObject(1), rs.getObject(2)));
-	}
-	public static JdbcRowSource<Tuple3<Object,Object,Object>> selectAsTuple3() {
-		return new JdbcRowSource<>(rs -> Tuple.of(rs.getObject(1), rs.getObject(2), rs.getObject(3)));
-	}
-	
-	public JdbcRowSource<T> from(JdbcProcessor proc) {
-		m_proc = proc;
-		return this;
-	}
-	
-	public JdbcRowSource<T> from(Connection conn) {
-		m_conn = conn;
-		return this;
-	}
-	
-	public JdbcRowSource<T> from(ResultSet rs) {
-		m_rs = rs;
-		return this;
-	}
-	
-	public JdbcRowSource<T> executeQuery(CheckedFunction<Connection, ResultSet> executor) {
-		m_exector = executor;
-		return this;
-	}
-	
-	public JdbcRowSource<T> executeQuery(String sql) {
-		m_exector = conn -> conn.createStatement().executeQuery(sql);
-		return this;
+	public static WaitRowSource<Tuple3<Object,Object,Object>> selectAsTuple3() {
+		return new WaitRowSource<>(rs -> Tuple.of(rs.getObject(1), rs.getObject(2), rs.getObject(3)));
 	}
 	
 	class OnSubscribe implements ObservableOnSubscribe<T> {
@@ -134,6 +111,7 @@ public class JdbcRowSource<T> {
 		@Override
 		public FOption<T> nextInGuard() {
 			try {
+				// 처음으로 레코드를 fetch할 때 SQL 처리를 수행하여 ResultSet 객체를 생성한다.
 				if ( m_rs == null ) {
 					if ( m_conn == null ) {
 						checkState(m_proc != null, "Connection has not set.");
@@ -144,6 +122,10 @@ public class JdbcRowSource<T> {
 				}
 				
 				return m_rs.next() ? FOption.of(m_deser.apply(m_rs)) : FOption.empty();
+			}
+			catch ( SQLException e ) {
+				Throwables.sneakyThrow(e);
+				throw new AssertionError("Should not be here");
 			}
 			catch ( Throwable e ) {
 				throw new FStreamException(e);
@@ -160,5 +142,55 @@ public class JdbcRowSource<T> {
 	
 	public FOption<T> first() {
 		return fstream().findFirst();
+	}
+	
+	public static class WaitRowSource<T> {
+		private final CheckedFunctionX<ResultSet, T, SQLException> m_deser;
+
+		private WaitRowSource(CheckedFunctionX<ResultSet, T, SQLException> deser) {
+			m_deser = deser;
+		}
+		
+		public WaitExecutor<T> from(JdbcProcessor proc) {
+			return new WaitExecutor<>(m_deser, proc, null, null);
+		}
+		
+		public WaitExecutor<T> from(Connection conn) {
+			return new WaitExecutor<>(m_deser, null, conn, null);
+		}
+		
+		public JdbcRowSource<T> from(ResultSet rs) {
+			return new JdbcRowSource<>(null, null, rs, null, m_deser);
+		}
+	}
+	
+	public static class WaitExecutor<T> {
+		private final CheckedFunctionX<ResultSet, T, SQLException> m_deser;
+		private final JdbcProcessor m_proc;
+		private final Connection m_conn;
+		private final ResultSet m_rs;
+
+		private WaitExecutor(CheckedFunctionX<ResultSet, T, SQLException> deser, JdbcProcessor proc,
+							Connection conn, ResultSet rs) {
+			m_deser = deser;
+			m_proc = proc;
+			m_conn = conn;
+			m_rs = rs;
+		}
+		
+		public JdbcRowSource<T> executeQuery(CheckedFunctionX<Connection, ResultSet, SQLException> executor) {
+			return new JdbcRowSource<>(m_proc, m_conn, m_rs, executor, m_deser);
+		}
+		
+		public JdbcRowSource<T> executeQuery(String sql) {
+			CheckedFunctionX<Connection, ResultSet, SQLException> exector = conn -> conn.createStatement().executeQuery(sql);
+			return new JdbcRowSource<>(m_proc, m_conn, m_rs, exector, m_deser);
+		}
+	}
+	
+	private static CheckedFunctionX<ResultSet, List<Object>, SQLException> deserializeAsObjectList() {
+		return rs -> FStream.range(1, rs.getMetaData().getColumnCount()+1)
+							.mapOrThrow(idx -> rs.getObject(idx))
+							.toList();
 	}
 }
