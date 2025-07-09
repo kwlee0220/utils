90cf73113a1df93919c7a90883fcdb8af002ea3c
Kang-Woo Lee <kwlee@etri.re.kr>
2025-07-09 10:21:30 +0900

    메소드 인자 순서 변경

--- src/main/java/utils/func/Unchecked.java
+++ src/main/java/utils/func/Unchecked.java
@@ -23,13 +23,13 @@ public class Unchecked {
 		}).get();
 	}
 
-	public static <T> void acceptOrIgnore(CheckedConsumer<? super T> checked, T data) {
+	public static <T> void acceptOrIgnore(T data, CheckedConsumer<? super T> checked) {
 		UncheckedConsumer.ignore(checked).accept(data);
 	}
-	public static <T> void acceptOrThrowSneakily(CheckedConsumer<? super T> checked, T data) {
+	public static <T> void acceptOrThrowSneakily(T data, CheckedConsumer<? super T> checked) {
 		checked.tryAccept(data).get();
 	}
-	public static <T> void acceptOrRTE(CheckedConsumer<? super T> checked, T data) {
+	public static <T> void acceptOrRTE(T data, CheckedConsumer<? super T> checked) {
 		checked.tryAccept(data).mapFailure(cause -> {
 			if ( cause instanceof RuntimeException ) {
 				return (RuntimeException)cause;
@@ -57,13 +57,13 @@ public class Unchecked {
 		}).get();
 	}
 
-	public static <T,R> R applyOrIgnore(CheckedFunction<? super T, ? extends R> checked, T input) {
+	public static <T,R> R applyOrIgnore(T input, CheckedFunction<? super T, ? extends R> checked) {
 		return UncheckedFunction.ignore(checked).apply(input);
 	}
-	public static <T,R> R applyOrThrowSneakily(CheckedFunction<? super T, ? extends R> checked, T input) {
+	public static <T,R> R applyOrThrowSneakily(T input, CheckedFunction<? super T, ? extends R> checked) {
 		return checked.tryApply(input).get();
 	}
-	public static <T,R> R applyOrRTE(CheckedFunction<? super T, ? extends R> checked, T input) {
+	public static <T,R> R applyOrRTE(T input, CheckedFunction<? super T, ? extends R> checked) {
 		return checked.tryApply(input).mapFailure(cause -> {
 			if ( cause instanceof RuntimeException ) {
 				return (RuntimeException)cause;
@@ -74,13 +74,13 @@ public class Unchecked {
 		}).get();
 	}
 
-	public static<T> boolean testOrIgnore(CheckedPredicate<? super T> checked, T input) {
+	public static<T> boolean testOrIgnore(T input, CheckedPredicate<? super T> checked) {
 		return UncheckedPredicate.ignore(checked).test(input);
 	}
-	public static <T> boolean testOrThrowSneakily(CheckedPredicate<? super T> checked, T input) {
+	public static <T> boolean testOrThrowSneakily(T input, CheckedPredicate<? super T> checked) {
 		return checked.tryTest(input).get();
 	}
-	public static <T> boolean testOrRTE(CheckedPredicate<? super T> checked, T input) {
+	public static <T> boolean testOrRTE(T input, CheckedPredicate<? super T> checked) {
 		return checked.tryTest(input).mapFailure(cause -> {
 			if ( cause instanceof RuntimeException ) {
 				return (RuntimeException)cause;
