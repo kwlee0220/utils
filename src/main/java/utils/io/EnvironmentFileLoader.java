0a5006a474e9604caaba73023aa962db45a6503c
Kang-Woo Lee <kwlee@etri.re.kr>
2026-03-25 17:53:15 +0900

    Add support for variable substitution with facts in EnvironmentFileLoader.

--- src/main/java/utils/io/EnvironmentFileLoader.java
+++ src/main/java/utils/io/EnvironmentFileLoader.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import utils.KeyValue;
 import utils.StrSubstitutor;
@@ -22,7 +23,15 @@ import utils.Utilities;
  */
 public class EnvironmentFileLoader {
 	private final File m_envFile;
+	private final Map<String,String> m_facts = Maps.newHashMap();
 	
+	/**
+	 * 환경 파일 로더를 생성한다.
+	 * 
+	 * @param envFile 환경 파일
+	 * @return 환경 파일 로더
+	 * @throws IOException 환경 파일을 읽는 과정에서 오류가 발생한 경우
+	 */
 	public static EnvironmentFileLoader from(File envFile) throws IOException {
 		return new EnvironmentFileLoader(envFile);
 	}
@@ -33,11 +42,33 @@ public class EnvironmentFileLoader {
 		m_envFile = envFile;
 	}
 	
-	public LinkedHashMap<String,String> load() throws IOException {
-		List<KeyValue<String, String>> keyValues = readEnvironmentFile(m_envFile);
-		return StrSubstitutor.replaceIncrementally(keyValues, Map.of());
+	/**
+	 * 환경 파일에서 참조하는 수 있는 변수들을 추가한다.
+	 * 
+	 * @param facts 변수 이름과 값의 맵
+	 * @return this
+	 */
+	public EnvironmentFileLoader withFacts(Map<String,String> facts) {
+		m_facts.putAll(facts);
+		return this;
 	}
 	
+	/**
+	 * 환경 파일을 읽어서 변수 치환을 수행한 결과를 반환한다.
+	 * 
+	 * @return 변수 치환이 완료된 환경 변수 맵
+	 * @throws IOException 환경 파일을 읽는 과정에서 오류가 발생한 경우
+	 */
+	public LinkedHashMap<String,String> load() throws IOException {
+		List<KeyValue<String, String>> keyValues = readEnvironmentFile(m_envFile);
+		return StrSubstitutor.replaceIncrementally(keyValues, m_facts);
+	}
+	
+	/**
+	 * 환경 파일에서 읽은 값을 이용하여 시스템 프로퍼티를 업데이트한다.
+	 * 
+	 * @throws IOException 환경 파일을 읽는 과정에서 오류가 발생한 경우
+	 */
 	public void updateSystemProperties() throws IOException {
 		for ( Map.Entry<String,String> e : load().entrySet() ) {
 			System.setProperty(e.getKey(), e.getValue());
